868bb0cfe8105db55f68c7e54bdc11741b444814
Idris Taoufiq <idris21@users.noreply.github.com>
2022-09-10 23:05:11 +0100

    Updated apiHost to match external ip of reverseproxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a72d1930b4ccd4a608428eb5e66a658a-1353356188.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /* 
